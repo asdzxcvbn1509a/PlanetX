43380ce1e59941ea85550915e470c53a4fc94eed
asdzxcvbn1509a <asdzxcvbn1509a@gmail.com>
2025-11-16 12:42:35 +0700

    Services Update

--- src/pages/Services/Illustration.jsx
+++ src/pages/Services/Illustration.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import IllustrationData from "../../data/IllustrationData";
 const Illustration = () => {
   return (
-    <div>Illustration</div>
-  )
-}
+    <div className="flex justify-center">
+      <div className="mt-[88px]">
+        <div className="flex mb-[32px] mt-[16px]">
+          <NavLink to="/" className="text-[#888888] text-xl">
+            หน้าหลัก
+          </NavLink>
+          <h1 className="text-[#888888] text-xl">&nbsp; &gt; &nbsp;</h1>
+          <NavLink to="/services" className="text-[#888888] text-xl">
+            บริการ
+          </NavLink>
+          <h1 className="text-[#888888] text-xl">&nbsp; &gt; &nbsp;</h1>
+          <NavLink to="/services/three-d" className="text-[#303030] text-xl">
+            Illustration
+          </NavLink>
+        </div>
+        <div>
+          <h1 className="text-[32px] text-[#493678] font-semibold mb-2">
+            ออกแบบ
+          </h1>
+          <hr className="text-[#303030]" />
+          <h1 className="text-xl mt-[13px] mb-6">Illustration</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-[50px]">
+          {IllustrationData.map((item) =>
+            item.type === "image" ? (
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-[472px] border shadow-lg"
+              />
+            ) : (
+              <video
+                autoPlay
+                loop
+                src={item.src}
+                className="w-[472px] border shadow-lg"
+              ></video>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Illustration
+export default Illustration;
--- src/pages/Services/Poster.jsx
+++ src/pages/Services/Poster.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import PosterData from "../../data/PosterData";
 
 const Poster = () => {
   return (
-    <div>Poster</div>
-  )
-}
+    <div className="flex justify-center">
+      <div className="mt-[88px]">
+        <div className="flex mb-[32px] mt-[16px]">
+          <NavLink to="/" className="text-[#888888] text-xl">
+            หน้าหลัก
+          </NavLink>
+          <h1 className="text-[#888888] text-xl">&nbsp; &gt; &nbsp;</h1>
+          <NavLink to="/services" className="text-[#888888] text-xl">
+            บริการ
+          </NavLink>
+          <h1 className="text-[#888888] text-xl">&nbsp; &gt; &nbsp;</h1>
+          <NavLink to="/services/three-d" className="text-[#303030] text-xl">
+            Illustration
+          </NavLink>
+        </div>
+        <div>
+          <h1 className="text-[32px] text-[#493678] font-semibold mb-2">
+            ออกแบบ
+          </h1>
+          <hr className="text-[#303030]" />
+          <h1 className="text-xl mt-[13px] mb-6">Illustration</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-[50px]">
+          {PosterData.map((item) =>
+            item.type === "image" ? (
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-[472px] border shadow-lg"
+              />
+            ) : (
+              <video
+                autoPlay
+                loop
+                src={item.src}
+                className="w-[472px] border shadow-lg"
+              ></video>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Poster
+export default Poster;
